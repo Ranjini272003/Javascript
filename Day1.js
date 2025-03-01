7e5dc0dcf8b3d83d872ec484550f9c5c9f741bcd
Ranjini272003 <ranjinimanickam27@gmail.com>
2025-03-01 13:57:43 +0530

    Day-1 Task

---
+++ Day1.js
@@ -0,0 +1,26 @@
+const name="Ranjini is my Name";
+console.log(name);
+
+let age=21;
+console.log(age);
+
+var likecoding = true;
+if ( likecoding) {
+    console.log("Like to Code")   
+}
+else{
+    console.log("Don't Likes to Code")
+}
+
+let likelang=new Array('Java','C','Javascript')
+console.log(likelang)
+
+let num1=10;
+let num2=20;
+console.log(num1);
+console.log(num2);
+console.log(num1+num2)
+console.log(num2-num1)
+console.log(num1*num2)
+console.log(num2/num1)
+console.log(num1%num2)
